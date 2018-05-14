838ae7872245fbbf8352854c367879ffa6713341
Anita Lew Kiedrowska <anitalk@poczta.onet.pl>
2018-05-14 19:53:25 +0200

    Add scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+var a = 2,
+    b = -3;
+var value = (a * a) - (2 * a * b) - (b * b);
+
+alert(value);
+if (value < 0) {
+  document.write('Wynik ujemny');
+} else if (value > 0) {
+  document.write('Wynik dodatni');
+} else if (value == 0) {
+  document.write('Wynik równy 0');
+}
